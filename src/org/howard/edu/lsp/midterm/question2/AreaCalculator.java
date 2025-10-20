7183542d6422372839738e8df20808a0c2ad84a3
Russell Langham Jr <rjlangham2004@gmail.com>
2025-10-20 19:07:46 -0400

    Q2: implment rectangle  area(double,double) implement triangle area(int, int)

--- src/org/howard/edu/lsp/midterm/question2/AreaCalculator.java
+++ src/org/howard/edu/lsp/midterm/question2/AreaCalculator.java
@@ -6,6 +6,18 @@ public class AreaCalculator {
         validatePositive(radius);
         return Math.PI * radius * radius;
     }
-    
+
+    //Rectangle Area
+    public static double area(doluble,width, double height){
+        validatePositive(width);
+        validatePositive(height);
+        return width * height;
+    }
+    //Triangle Area
+    public static double area(int base, int height){
+        validatePositive(base);
+        validatePositive(height);
+        return 0.5 * base * height;
+    }
     
 }
