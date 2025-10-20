dad3e2a8f05ecc6dc2db186ffac271316f2539de
Russell Langham Jr <rjlangham2004@gmail.com>
2025-10-20 19:04:37 -0400

    Q2: implment circle area(double)

--- src/org/howard/edu/lsp/midterm/question2/AreaCalculator.java
+++ src/org/howard/edu/lsp/midterm/question2/AreaCalculator.java
@@ -1,5 +1,11 @@
 package org.howard.edu.lsp.midterm.question2;
 
 public class AreaCalculator {
+    //Circle Area
+    public static double area(double radius){
+        validatePositive(radius);
+        return Math.PI * radius * radius;
+    }
+    
     
 }
